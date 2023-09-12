e5fd9f9b161f73dc0c82894f38453010dd6aa0b4
mr-chidex <mbahchiboy2@gmail.com>
2023-09-12 16:03:03 +0100

    init app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import "dotenv/config";
-import "reflect-metadata";
-import cors from "cors";
+import express from 'express';
+import 'dotenv/config';
+import 'reflect-metadata';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 // app.use(morgan('dev'));
 
 export default app;
